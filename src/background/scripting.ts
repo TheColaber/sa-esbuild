31773aae2d1d8470f198a1b6921fb85c5e249ac7
TheColaber <thecolaber@gmail.com>
2024-01-21 00:11:57 -0500

    crazy town

--- src/background/scripting.ts
+++ src/background/scripting.ts
@@ -1,40 +1,44 @@
 import { syncStorage, addonEnabledStates } from "./storage";
+const tabIds = [];
+chrome.tabs.query({}).then(tabs => {
+  tabIds.push(...tabs.filter((tab) => tab.url).map(tab => tab.id))
+})
 
 chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab) => {
   if (!tab.url || status !== "loading") return;
+  tabIds.push(tabId);
 
   let { addonsStates } = await syncStorage.get("addonsStates");
   const userLangs = await getUserLangs(tab.url);
 
-  dispatch("addonData", { addonsStates, addonEnabledStates, userLangs });
+  dispatch("addonData", { addonsStates, addonEnabledStates, userLangs }, tabId);
+});
 
-  syncStorage.watch(["addonsStates"], ({ addonsStates: {oldValue, newValue} }) => {        
-    for (const id in newValue) {
-      if (oldValue[id] !== newValue[id]) {
-        console.log(newValue[id],addonEnabledStates);
-        
+syncStorage.watch(["addonsStates"], ({ addonsStates: {oldValue, newValue} }) => {        
+  for (const id in newValue) {
+    if (oldValue[id] !== newValue[id]) {      
+      for (const tabId of tabIds) {
         if (addonEnabledStates.some((state) => newValue[id] === state)) {
-          dispatch("dynamicEnable", { id });
+          dispatch("dynamicEnable", { id }, tabId);
         } else {
-          dispatch("dynamicDisable", { id });
+          dispatch("dynamicDisable", { id }, tabId);
         }
       }
     }
-    addonsStates = newValue;
-  });
-
-  function dispatch(type: string, detail: any) {
-    chrome.scripting.executeScript({
-      target: { tabId },
-      injectImmediately: process.env.MODE !== "development",
-      world: "MAIN",
-      func: async (type, detail: any) => {        
-        scratchAddons.events.dispatchEvent(new CustomEvent(type, { detail }));
-      },
-      args: [type, detail],
-    });
   }
 });
+
+function dispatch(type: string, detail: any, tabId: number) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    injectImmediately: process.env.MODE !== "development",
+    world: "MAIN",
+    func: async (type, detail: any) => {        
+      scratchAddons.events.dispatchEvent(new CustomEvent(type, { detail }));
+    },
+    args: [type, detail],
+  });
+}
 
 async function getUserLangs(url: string) {
   const cookie = await chrome.cookies.get({ url, name: "scratchlanguage" });
