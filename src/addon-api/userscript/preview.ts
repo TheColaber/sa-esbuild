92e9c6919be84ab2b9d2046a516aa44b4857e7f2
TheColaber <thecolaber@gmail.com>
2024-06-10 07:24:00 -0400

    worlk

--- src/addon-api/userscript/preview.ts
+++ src/addon-api/userscript/preview.ts
@@ -1,6 +1,6 @@
 export default class Preview extends EventTarget {
-  createEditorTooltip(content: string, place: "left"|"right"|"bottom"|"top" = "bottom") {
-    const tooltipDiv = document.createElement("div");
+  createEditorTooltip(content: string, attach: Element, place: "left"|"right"|"bottom"|"top" = "bottom") {
+    const tooltipDiv = document.body.appendChild(document.createElement("div"));
     const list = [
       "__react_component_tooltip",
       "place-" + place,
@@ -10,11 +10,20 @@ export default class Preview extends EventTarget {
       "coming-soon_" + place + "_2raz4",
       "show"]
     tooltipDiv.className = list.join(" ");
-    tooltipDiv.style.position = "relative";
-    tooltipDiv.style.top = "0px";
-    tooltipDiv.style.left = "0px";
     const tooltipSpan = tooltipDiv.appendChild(document.createElement("span"));
     tooltipSpan.textContent = content;
+    const rect = attach.getBoundingClientRect();
+    let top = 0;
+    let left = 0
+    if (place === "bottom") {      
+      top = rect.bottom;
+      left = rect.left;
+    } else if (place === "right") {
+      top = rect.top;
+      left = rect.right;
+    }
+    tooltipDiv.style.top = top + "px";
+    tooltipDiv.style.left = left + "px";
     return tooltipDiv;
   }
 }
--- src/background/scripting.ts
+++ src/background/scripting.ts
@@ -56,6 +56,7 @@ addonStorage.watch((changes) => {
   }
 });
 
+const SA_EDITOR_PROJECT = 1034606782;
 onPortConnection((port) => {
   port.onMessage("getRunningAddons", async () => {    
     const [tab] = await chrome.tabs.query({
@@ -79,7 +80,7 @@ onPortConnection((port) => {
   });
   port.onMessage("openScratchEditor", async (data) => {
     const tab = await chrome.tabs.create({
-      url: "https://scratch.mit.edu/projects/editor",
+      url: "https://scratch.mit.edu/projects/" + SA_EDITOR_PROJECT +"/editor",
     });
     previewTabs[tab.id] = data;
   });
